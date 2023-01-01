d32558d105bb51fb193cb47da28896a2c6415e0c
Stefan <sjvonstein@gmail.com>
2023-01-01 09:32:00 -0500

    switched to postgres server

--- website/__init__.py
+++ website/__init__.py
@@ -3,13 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "cornhole"
 
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "dgf987324lkjsdef098lkjadf"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:h4decAsT@localhost/cornhole"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
     from .views import views
